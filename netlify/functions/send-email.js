b0ed11afa9fbdfc71d04e18fcd7fa0276fdfc073
Lukasz-Woloszyn <77557510+Lukasz-Woloszyn@users.noreply.github.com>
2024-09-12 20:51:30 +0200

    Update send-email.js

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,48 +1,50 @@
 import axios from "axios";
-import cors from "cors";
 
 const apiToken = import.meta.env.API_TOKEN;
 const url = import.meta.env.API_URL;
 
-const corsHandler = cors({
-  origin: "https://bpmonitor.netlify.app", // Zamień na URL swojej aplikacji
-});
+const axios = require('axios');
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   const { userId, toEmail, userEmail, level, systolicValue, diastolicValue, pulseValue, date, time } = JSON.parse(event.body);
-  
-  // Szablony wiadomości
+  const apiToken = import.meta.env.API_TOKEN;
+  const url = import.meta.env.API_URL;
+
   const emailTemplates = [
     `Alert! Hypotension detected!\nUser: ${userEmail}\nSystolic Pressure: ${systolicValue}\nDiastolic Pressure: ${diastolicValue}\nPulse: ${pulseValue}\nDate: ${date}\nTime: ${time}`,
     `Alert! Prehypertension detected!\nUser: ${userEmail}\nSystolic Pressure: ${systolicValue}\nDiastolic Pressure: ${diastolicValue}\nPulse: ${pulseValue}\nDate: ${date}\nTime: ${time}`,
     `Alert! Stage 1 Hypertension detected!\nUser: ${userEmail}\nSystolic Pressure: ${systolicValue}\nDiastolic Pressure: ${diastolicValue}\nPulse: ${pulseValue}\nDate: ${date}\nTime: ${time}`,
     `Alert! Stage 2 Hypertension detected!\nUser: ${userEmail}\nSystolic Pressure: ${systolicValue}\nDiastolic Pressure: ${diastolicValue}\nPulse: ${pulseValue}\nDate: ${date}\nTime: ${time}`,
-    `Alert! Hypertensive Crisis detected!\nUser: ${userEmail}\nSystolic Pressure: ${systolicValue}\nDiastolic Pressure: ${diastolicValue}\nPulse: ${pulseValue}\nDate: ${date}\nTime: ${time}`,
+    `Alert! Hypertensive Crisis detected!\nUser: ${userEmail}\nSystolic Pressure: ${systolicValue}\nDiastolic Pressure: ${diastolicValue}\nPulse: ${pulseValue}\nDate: ${date}\nTime: ${time}`
   ];
 
   const emailContent = emailTemplates[level - 1] || "";
 
   try {
-    const response = await axios.post(url, {
-      from: {
-        email: 'alerts@bpmonitor.com',
-        name: 'Alerts',
+    const response = await axios.post(
+      url,
+      {
+        from: {
+          email: "alerts@bpmonitor.com",
+          name: "Alerts",
+        },
+        to: [{ email: toEmail }],
+        subject: "Blood Pressure Alert",
+        text: emailContent,
       },
-      to: [{ email: toEmail }],
-      subject: 'Blood Pressure Alert',
-      text: emailContent,
-    }, {
-      headers: {
-        Authorization: `Bearer ${apiToken}`,
-        'Content-Type': 'application/json',
-      },
-    });
-
+      {
+        headers: {
+          Authorization: `Bearer ${apiToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
     return {
       statusCode: 200,
       body: JSON.stringify(response.data),
     };
   } catch (error) {
+    console.error("Error sending an email:", error.message);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
